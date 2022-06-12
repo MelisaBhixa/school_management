b1891d6fb1e98e6de1d431bd85bc27fbce288cb1
legiste99 <217046207@mycput.ac.za>
2022-06-12 16:52:52 +0200

    domain - Employee, EmployeeAddress complete

--- src/main/java/za/ac/cput/domain/lookup/EmployeeAddress.java
+++ src/main/java/za/ac/cput/domain/lookup/EmployeeAddress.java
@@ -1,4 +1,52 @@
+/**
+ EmployeeAddress.java
+ Entity for the EmployeeAddress
+ Author: Legiste Ndabashinze (217046207)
+ Date: 12 June 2022
+ */
 package za.ac.cput.domain.lookup;
 
+import za.ac.cput.domain.location.Address;
+
 public class EmployeeAddress {
+    private String staffId;
+    private Address address;
+
+    public EmployeeAddress(EmployeeAddressBuilder employeeAddressBuilder) {
+        this.staffId = employeeAddressBuilder.staffId;
+        this.address = employeeAddressBuilder.address;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public static class EmployeeAddressBuilder{
+        private String staffId;
+        private Address address;
+
+        public EmployeeAddressBuilder setStaffId(String staffId) {
+            this.staffId = staffId;
+            return this;
+        }
+
+        public EmployeeAddressBuilder setAddress(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public EmployeeAddressBuilder copy(EmployeeAddress employeeAddress){
+            this.staffId = employeeAddress.staffId;
+            this.address = employeeAddress.address;
+            return this;
+        }
+
+        public EmployeeAddress build(){
+            return new EmployeeAddress(this);
+        }
+    }
 }
--- src/main/java/za/ac/cput/domain/users/Employee.java
+++ src/main/java/za/ac/cput/domain/users/Employee.java
@@ -1,4 +1,64 @@
+/**
+ Employee.java
+ Entity for the Employee
+ Author: Legiste Ndabashinze (217046207)
+ Date: 12 June 2022
+ */
 package za.ac.cput.domain.users;
 
+import za.ac.cput.domain.user_info.Name;
+
 public class Employee {
+    private String staffId, email;
+    private Name name;
+
+    public Employee(EmployeeBuilder employeeBuilder) {
+        this.staffId = employeeBuilder.staffId;
+        this.email = employeeBuilder.email;
+        this.name = employeeBuilder.name;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public static class EmployeeBuilder{
+
+        private String staffId, email;
+        private Name name;
+
+        public EmployeeBuilder setStaffId(String staffId) {
+            this.staffId = staffId;
+            return this;
+        }
+
+        public EmployeeBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public EmployeeBuilder setName(Name name) {
+            this.name = name;
+            return this;
+        }
+
+        public EmployeeBuilder copy(Employee employee){
+            this.staffId = employee.staffId;
+            this.email = employee.email;
+            this.name = employee.name;
+            return this;
+        }
+
+        public Employee build(){
+            return new Employee(this);
+        }
+    }
 }
