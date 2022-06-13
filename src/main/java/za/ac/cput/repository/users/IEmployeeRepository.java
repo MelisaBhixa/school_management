0f26e0b6a5dead29ecc54a9b746a5189e83c5681
legiste99 <217046207@mycput.ac.za>
2022-06-14 01:54:26 +0200

    implemented repository layer for Employee and EmployeeAddress

--- src/main/java/za/ac/cput/repository/users/IEmployeeRepository.java
+++ src/main/java/za/ac/cput/repository/users/IEmployeeRepository.java
@@ -1,4 +1,18 @@
+/**
+ IEmployeeRepository.java
+ Repository interface for the Employee
+ Author: Legiste Ndabashinze (217046207)
+ Date: 14 June 2022
+ */
 package za.ac.cput.repository.users;
 
-public interface IEmployeeRepository {
+import za.ac.cput.domain.users.Employee;
+import za.ac.cput.repository.IRepository;
+
+import java.util.List;
+import java.util.Set;
+
+public interface IEmployeeRepository extends IRepository<Employee, String> {
+    List<Employee> getAll();
+    List<Employee> findByStaffId(String staffId);
 }
