8c44a2e8bc62f87c103fdb57624ba088560ff6de
PanasheMuinzani <sharondesigns35@gmail.com>
2022-06-13 23:06:09 +0200

    City Domain and Factory

--- src/main/java/za/ac/cput/factory/location/CityFactory.java
+++ src/main/java/za/ac/cput/factory/location/CityFactory.java
@@ -1,4 +1,23 @@
+/*
+ * City Factory
+ * CityFactory.java
+ * Author: Panashe Muinzani (218186568)
+ * 13 June 2022
+ */
+
 package za.ac.cput.factory.location;
 
+import za.ac.cput.domain.location.City;
+import za.ac.cput.domain.location.Country;
+
 public class CityFactory {
+
+    public static City createCity (String id, String name, Country country){
+
+        return new City.CityBuilder()
+                .setId(id)
+                .setName(name)
+                .setCountry(country)
+                .build();
+    }
 }
