8cbf208ba7912c7aaaa85a63d4b86dc152d4584a
Thina Mbiza <thina@vicinity-media.com>
2022-06-13 18:01:29 +0200

    Thina Country Factory Commit

--- src/main/java/za/ac/cput/factory/location/CountryFactory.java
+++ src/main/java/za/ac/cput/factory/location/CountryFactory.java
@@ -1,4 +1,23 @@
+/*
+ * Country Factory
+ * CountryFactory.java
+ * Thina Mbiza 217217095
+ * 12 June 2022
+ */
+
 package za.ac.cput.factory.location;
 
+import za.ac.cput.domain.location.Country;
+
 public class CountryFactory {
+
+    public static Country createCountry (String id, String name){
+
+        return new Country.Builder()
+                .setId(id)
+                .setName(name)
+                .build();
+    }
 }
+
+
