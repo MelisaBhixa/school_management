3e09e152dd821cb3159d46d1486025ffb5dbbc71
ZFukula <217299911@mycput.ac.za>
2022-06-14 23:11:30 +0200

    Respective Repository and Factory classes added Zuko Fukula 217299911

--- src/main/java/za/ac/cput/factory/lookup/StudentAddressFactory.java
+++ src/main/java/za/ac/cput/factory/lookup/StudentAddressFactory.java
@@ -1,4 +1,24 @@
+/**
+ StudentAddressFactory.java
+ Entity for StudentAddressFactory
+ Author: Zuko Fukula (217299911)
+ Date: 13 June 2022
+ */
+
 package za.ac.cput.factory.lookup;
 
+import za.ac.cput.domain.location.Address;
+import za.ac.cput.domain.lookup.StudentAddress;
+import za.ac.cput.util.Helper;
+
 public class StudentAddressFactory {
+
+    public static StudentAddress newStudentAddress(String studentID, Address address){
+        Helper.checkStringParam("studentID", studentID);
+        Helper.isObjectNull(address);
+
+    return new StudentAddress.Builder().setStudentID(studentID).setAddress(address).build();
+
+
+    }
 }
--- src/main/java/za/ac/cput/repository/users/IStudentRepository.java
+++ src/main/java/za/ac/cput/repository/users/IStudentRepository.java
@@ -1,4 +1,17 @@
+/**
+ IStudentRepository.java
+ Entity for IStudentRepository
+ Author: Zuko Fukula (217299911)
+ Date: 13 June 2022
+ */
+
+
 package za.ac.cput.repository.users;
 
-public interface IStudentRepository {
+import za.ac.cput.domain.users.Student;
+import za.ac.cput.repository.IRepository;
+import java.util.Set;
+
+public interface IStudentRepository extends IRepository<Student, String> {
+    Set<Student> getAll();
 }
