4c9fa0556dd6aa21f116b4bcd180d3bcaf0ee438
Melisa <217131085@mycput.ac.za>
2022-06-15 19:40:44 +0200

    Address, AddressRepository, AddressFactory

--- src/main/java/za/ac/cput/repository/location/IAddressRepository.java
+++ src/main/java/za/ac/cput/repository/location/IAddressRepository.java
@@ -1,4 +1,17 @@
+/*
+AddressIRepository.java
+IRepository for Address
+Author: Melisa Bhixa
+Date: 15 June 2022
+ */
+
 package za.ac.cput.repository.location;
 
-public interface IAddressRepository {
+import za.ac.cput.domain.location.Address;
+import za.ac.cput.repository.IRepository;
+import java.util.List;
+
+
+public interface IAddressRepository extends IRepository<Address, String> {
+    List<Address> getAll();
 }
