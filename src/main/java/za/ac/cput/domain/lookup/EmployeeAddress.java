cf115a4029c42d820dece925c6fac24391d3f3b9
legiste99 <217046207@mycput.ac.za>
2022-06-18 14:24:16 +0200

    updated Employee,EmployeeAddress and Name classes

--- src/main/java/za/ac/cput/domain/lookup/EmployeeAddress.java
+++ src/main/java/za/ac/cput/domain/lookup/EmployeeAddress.java
@@ -8,9 +8,21 @@ package za.ac.cput.domain.lookup;
 
 import za.ac.cput.domain.location.Address;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
 public class EmployeeAddress {
-    private final String staffId;
-    private final Address address;
+    @NotNull @Id
+    private String staffId;
+    @Embedded
+    private Address address;
+
+    protected EmployeeAddress() {
+    }
 
     private EmployeeAddress(EmployeeAddressBuilder employeeAddressBuilder) {
         this.staffId = employeeAddressBuilder.staffId;
@@ -51,6 +63,19 @@ public class EmployeeAddress {
             this.staffId = employeeAddress.staffId;
             this.address = employeeAddress.address;
             return this;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            EmployeeAddress that = (EmployeeAddress) o;
+            return staffId.equals(that.staffId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(staffId);
         }
 
         public EmployeeAddress build(){
